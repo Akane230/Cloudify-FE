fe641e8cadaebb2b9594f0988dfadbe80c984891
Isabella Gonzales <gonzales.isabella1414@gmail.com>
2026-01-17 23:49:14 +0800

    partial settings ui

--- app/pages/settings.jsx
+++ app/pages/settings.jsx
@@ -5,6 +5,11 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import BackIcon from '../../assets/icons/backIcon';
 import UserInfoHeader from '../components/userInfoHeader';
 import api from '../../config/api';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import SettingsIcon from '../../assets/icons/settingsIcon';
+import MessageIconInactive from '../../assets/icons/messageIconInactive';
+import FriendreqIcon from '../../assets/icons/friendreqIcon';
+import ArchiveIcon from '../../assets/icons/archiveIcon';
 
 const Settings = () => {
   const router = useRouter();
@@ -72,6 +77,29 @@ const Settings = () => {
           </View>
         )}
 
+        <View style={styles.menuContainer}>
+          <TouchableOpacity style={styles.menuCard}>
+            <SettingsIcon width={24} height={24} />
+            <Text style={styles.menuText}>Settings</Text>
+          </TouchableOpacity>
+
+        </View>
+
+        <TouchableOpacity style={styles.menuCard}>
+          <MessageIconInactive width={24} height={24} />
+          <Text style={styles.menuText}>Message requests</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.menuCard}>
+          <FriendreqIcon width={24} height={24} />
+          <Text style={styles.menuText}>Friend requests</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.menuCard}>
+          <ArchiveIcon width={24} height={24} />
+          <Text style={styles.menuText}>Archive</Text>
+        </TouchableOpacity>
+
       </ScrollView>
     </SafeAreaView>
   );
@@ -107,5 +135,18 @@ const styles = StyleSheet.create({
   },
   userInfoContainer: {
     paddingBottom: 40,
+  },
+  menuCard: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#262729',
+    borderRadius: 10,
+    padding: 15,
+    marginBottom: 10,
+  },
+  menuText: {
+    color: 'white',
+    fontSize: 16,
+    marginLeft: 15,
   },
 });
